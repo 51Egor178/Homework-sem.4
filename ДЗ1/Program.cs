955fb7cff72b4bd5c7a57f4a2bf3dfa7934f2aed
Egor <vev_113@mail.ru>
2024-01-18 21:22:28 +0300

    Бесконечный запрос чисел до ввода q или числа с четнойсуммой цифр

---
+++ ДЗ1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Введите целое число или 'q' для завершения: ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "q")
+            {
+                Console.WriteLine("Программа завершена.");
+                break;
+            }
+
+            if (IsEvenDigitSum(int.Parse(input)))
+            {
+                Console.WriteLine($"Сумма цифр числа {input} чётная. Программа завершена.");
+                break;
+            }
+        }
+    }
+
+    static bool IsEvenDigitSum(int number)
+    {
+        int sum = 0;
+        while (number != 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum % 2 == 0;
+    }
+}
+
