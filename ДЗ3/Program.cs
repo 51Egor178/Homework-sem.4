7752d0d10a7350882eb6aca2b882f71f807ea94a
Egor <vev_113@mail.ru>
2024-01-18 23:35:03 +0300

    Реверсирование одномерного массива

---
+++ ДЗ3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем массив
+        int[] originalArray = { 1, 2, 3, 4, 5 };
+
+        // Выводим исходный массив
+        Console.WriteLine("Исходный массив:");
+        PrintArray(originalArray);
+
+        // Переворачиваем массив
+        int[] reversedArray = ReverseArray(originalArray);
+
+        // Выводим перевернутый массив
+        Console.WriteLine("Перевернутый массив:");
+        PrintArray(reversedArray);
+    }
+
+    static int[] ReverseArray(int[] array)
+    {
+        int length = array.Length;
+        int[] reversedArray = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            reversedArray[i] = array[length - 1 - i];
+        }
+
+        return reversedArray;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (var element in array)
+        {
+            Console.Write($"{element} ");
+        }
+        Console.WriteLine();
+    }
+}
+
